f66dea29ee4666aaa30c17737d9fe5e0278a1c18
Brandon Ung <b1ung@ucsd.edu>
2018-11-07 13:22:10 -0800

    made profile link a button on message board page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,11 @@ function logoutConfirm() {
     var txt;
     if (confirm("Are you sure you want to logout?")) {
         window.location="./login.html"
-    } else {
-        txt = "You pressed Cancel!";
     }
+    
     document.getElementById("demo").innerHTML = txt;
+}
+
+function goProfile() {
+    window.location="./profile.html"
 }
