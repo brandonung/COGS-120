b12fc9072283ad30522e4b7c1aec237759883e31
Brandon Ung <b1ung@ucsd.edu>
2018-11-08 22:14:22 -0800

    added eventcode popup, made basic profile page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,39 @@ function logoutConfirm() {
     document.getElementById("demo").innerHTML = txt;
 }
 
+function goHome() {
+    window.location="./index.html"
+}
+
 function goProfile() {
     window.location="./profile.html"
 }
+
+function promptEventCode() {
+    var code = prompt("Please enter your event code", "###-###")
+    if (code) {
+        window.location="./mboard.html"
+    } else {
+        return null;
+    }
+
+}
+
+function showModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+}
+
+function hideModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "none";
+}
+
+window.addEventListener('load', function() {
+    var modal = document.getElementById('myModal');
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+})
