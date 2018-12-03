67fce0d92e8380f9cbac8a6e3494a3aafde0d1d0
Brandon Ung <b1ung@ucsd.edu>
2018-12-03 14:51:02 -0800

    working on localstorage database

---
+++ scripts/database.js
@@ -0,0 +1,13 @@
+var userProfiles = [
+    {'name': 'Spiderman', 'profilePic': 'images/spiderman.jpg', 'index': 1},
+    {'name': 'Barney', 'profilePic': 'images/barney.jpg', 'index': 2},
+    
+]
+
+var messageBoard = {
+
+}
+
+var messages = {
+    
+}
