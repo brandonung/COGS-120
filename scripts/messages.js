1cd4a29582e32b8d3a0c19defead8dae8796d783
Brandon Ung <b1ung@ucsd.edu>
2018-12-06 22:30:53 -0800

    changed nav bars on all pages; can send more than one msg

--- scripts/messages.js
+++ scripts/messages.js
@@ -9,33 +9,49 @@ $(document).ready(function(){
 
 function appendMsg() {
     var node = document.createElement("DIV");
+    var inner = document.createElement("DIV");
     var pg = document.createElement("P");
-    node.setAttribute("class", "outgoing_msg sent_msg");
+    var currtime = document.createElement("SPAN");
+
+    node.setAttribute("class", "outgoing_msg");
+    inner.setAttribute("class", "sent_msg");
     var input = document.getElementById("newMsg");
     var textnode = document.createTextNode(input.value);
     pg.appendChild(textnode);
-    node.appendChild(pg);
+    inner.appendChild(pg);
+    
+    currtime.setAttribute("class", "time_date");
+    inner.appendChild(currtime);
+    node.appendChild(inner);
+
+    startTime(currtime);
+
     document.getElementById("chatBody").appendChild(node);
     document.getElementById("newMsg").value='';
-  //  messages.push(text);
-  //  localStorage.setItem("messages", JSON.stringify(messages));
 }
 
-/*function loadMessages() {
-    if (localStorage.getItem("messages") !== null) {
-        var stored_msgs = JSON.parse(localStorage.getItem("messages"));
-        
-        for(i=0; i<stored_msgs.length; i++) {
-            var node = document.getElementById("DIV");
-            var pg = document.createElement("P");
-            node.setAttribute("class", "outgoing_msg sent_msg");
-            pg.appendChild(stored_msgs[i]);
-            node.appendChild(pg);
-            document.getElementById("chatBody").appendChild(node);
-            document.getElementById("newMsg").value='';
-        }
-    
-    } else {
-        return;
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
     }
-}*/
+    return i;
+}
+  
+function startTime(currtime) {
+    var today = new Date();
+    var ampm = "AM";
+    var h = today.getHours();
+    if (h == 0) {
+        h = 12;
+    } else if (h > 12) {
+        h -= 12;
+        ampm = "PM";
+    }
+    var m = today.getMinutes();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    currtime.innerHTML = h + ":" + m + ampm;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+}
